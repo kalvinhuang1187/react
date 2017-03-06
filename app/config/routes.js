346d36c28714a4e06537604c1c09621f30ae3f77
Kalvin Huang <kalvin_huang@yahoo.com>
2017-03-06 14:16:24 -0800

    Add homepage

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,7 +3,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
-var hashHistory = ReactRouter.hashHistory
+var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
 var PromptContainer = require('../containers/PromptContainer');
@@ -12,7 +12,7 @@ var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
-      <Route path='playerOne' component={PromptContainer} />
+      <Route path='playerOne' header='Player One' component={PromptContainer} />
       <Route path='playerTwo/:playerOne' component={PromptContainer} />
     </Route>
   </Router>
