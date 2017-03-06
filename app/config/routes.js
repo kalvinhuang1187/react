0305a4e59c71900e629a60eb51fe2dcd248a9f09
Kalvin Huang <kalvin_huang@yahoo.com>
2017-03-06 14:35:33 -0800

    Build playerOne and playerTwo pages

--- app/config/routes.js
+++ app/config/routes.js
@@ -13,7 +13,7 @@ var routes = (
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
       <Route path='playerOne' header='Player One' component={PromptContainer} />
-      <Route path='playerTwo/:playerOne' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='PlayerTwo' component={PromptContainer} />
     </Route>
   </Router>
 );
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -2,16 +2,57 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 
 var PromptContainer = React.createClass({
+  contextTypes: {
+    //dont have to pass router through props
+    router: React.PropTypes.object.isRequired
+  },
+
+  getInitialState: function() {
+    return {
+      username: ''
+    }
+  },
+
+  onUpdateUser: function(e) {
+    this.setState({
+      username: e.target.value
+    })
+  },
+
+  onSubmitUser: function (e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
+
+    if (this.props.routeParams.playerOne) {
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    } else {
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
+
+  //presenting UI of component
   render: function () {
     return (
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
         <h1>{this.props.route.header}</h1>
         <div className="col-sm-12">
-          <form>
+          <form onSubmit={this.onSubmitUser}>
             <div className="form-group">
               <input
                 className="form-control"
                 placeholder="Github Username"
+                //username on change will update value
+                onChange={this.onUpdateUser}
+                value={this.state.username}
                 type="text" />
             </div>
             <div className="form-group col-sm-4 col-sm-offset-4">
