41188a16df494898adf57f01543bacd992357961
Kalvin Huang <kalvin_huang@yahoo.com>
2017-03-07 15:54:12 -0800

    Promises for player one and two github info

---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,24 @@
+var axios = require('axios');
+
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var params = "?client_id=" + id + "&client_secret=" + sec;
+
+function getUserInfo(username) {
+  return axios.get('https://api.github.com/users/' + username + params)
+}
+
+var helpers = {
+  getPlayersInfo: function(players) {
+    // loop over ever username in players array. for each user call getUserInfo
+    return axios.all(players.map(function (username) {
+      return getUserInfo(username)
+    })).then(function (info) {
+      return info.map(function (user) {
+        return user.data
+      })
+    })
+  }
+};
+
+module.exports = helpers;
