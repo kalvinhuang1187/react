7145405e7a0788521d1901e078ea86b2f44af183
Kalvin Huang <kalvin_huang@yahoo.com>
2017-03-09 11:17:18 -0800

    Refactor- pull out user detail into a wrapper

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,6 +4,7 @@ var styles = require('../styles');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function puke(object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -14,15 +15,13 @@ function ConfirmBattle (props) {
     ? <p> LOADING! </p>
     : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
-        <div className='col-sm-8 col-sm-offset-2'>
-          <div className='col-sm-6'>
-            <p className='lead'>Player 1</p>
+        <div className='col-sm-8 col-sm-offset-2'
+          <UserDetailsWrapper header='Player One'>
             <UserDetails info={props.playersInfo[0]} />
-          </div>
-          <div className='col-sm-6'>
-            <p className='lead'>PLAYER 2</p>
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player Two'>
             <UserDetails info={props.playersInfo[1]} />
-          </div>
+          </UserDetailsWrapper>
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
           <div className='col-sm-12' style={styles.space}>
