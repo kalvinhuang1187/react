140e27a32e8cdfa8f045b5cf5ef3c774a4d6041f
Kalvin Huang <kalvin_huang@yahoo.com>
2017-03-10 14:09:47 -0800

    Refactor- move transparentBg to MainContainer

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,6 +4,7 @@ var styles = require('../styles');
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = require('react-router').Link;
+var MainContainer = require('./MainContainer');
 
 function puke (obj) {
   return <pre>{JSON.stringify(obj, 2, ' ')} </pre>
@@ -20,12 +21,18 @@ function StartOver() {
 }
 
 function Results (props) {
+  if (props.isLoading === true) {
+    return (
+      <p> LOADING </p>
+    )
+  }
+
   if (props.scores[0] === props.scores[1]) {
     return (
-      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <MainContainer>
         <h1>Its a tie!</h1>
         <StartOver />
-      </div>
+      </MainContainer>
     )
   }
 
@@ -33,7 +40,7 @@ function Results (props) {
   var losingIndex = winningIndex === 0 ? 1 : 0;
 
   return (
-    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+    <MainContainer>
       <h1>Results</h1>
       <div className='col-sm-8 col-sm-offset-2'>
         <UserDetailsWrapper header='Winner'>
@@ -44,7 +51,7 @@ function Results (props) {
         </UserDetailsWrapper>
       </div>
       <StartOver />
-    </div>
+    </MainContainer>
   )
 }
 
